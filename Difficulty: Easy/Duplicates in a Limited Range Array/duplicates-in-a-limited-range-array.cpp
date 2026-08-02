827a854e138840c4c461d4421a1caecaa6c816df
Rudraksh Kumar <rudrakk2005@gmail.com>
2026-08-02 22:01:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Duplicates in a Limited Range Array/duplicates-in-a-limited-range-array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    vector<int> findDuplicates(vector<int>& arr) {
+        // code here
+        vector<int> ans;
+        
+        for(int i=0; i<arr.size(); i++){
+            int index = abs(arr[i])-1;
+            if(arr[index]<0){
+                ans.push_back(index+1);
+            }
+            else{
+                arr[index] = -arr[index];
+            }
+        }
+        return ans;
+    }
+};
