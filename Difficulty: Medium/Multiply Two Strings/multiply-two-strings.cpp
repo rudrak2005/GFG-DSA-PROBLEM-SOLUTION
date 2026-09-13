ea2bd03045b0491b1f7e9ecaf139e04605ad6690
Rudraksh Kumar <rudrakk2005@gmail.com>
2026-09-13 21:49:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Multiply Two Strings/multiply-two-strings.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    string multiplyStrings(string &s1, string &s2) {
+
+        bool neg = false;
+
+        if (s1[0] == '-') {
+            neg = !neg;
+            s1 = s1.substr(1);
+        }
+        if (s2[0] == '-') {
+            neg = !neg;
+            s2 = s2.substr(1);
+        }
+
+        // Remove leading zeros
+        while (s1.size() > 1 && s1[0] == '0') s1.erase(0, 1);
+        while (s2.size() > 1 && s2[0] == '0') s2.erase(0, 1);
+
+        if (s1 == "0" || s2 == "0") return "0";
+
+        int n = s1.size(), m = s2.size();
+        vector<int> res(n + m, 0);
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 0; j--) {
+                int sum = (s1[i] - '0') * (s2[j] - '0') + res[i + j + 1];
+                res[i + j + 1] = sum % 10;
+                res[i + j] += sum / 10;
+            }
+        }
+
+        string ans = "";
+        int i = 0;
+        while (i < res.size() && res[i] == 0) i++;
+
+        while (i < res.size()) ans += char(res[i++] + '0');
+
+        if (neg) ans = "-" + ans;
+        return ans;
+    }
+};
